2a344c4a7a06eaceb700e05fbec89bcd1b8fb9e7
Rafael Haone <haone95@gmail.com>
2023-10-25 11:18:10 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: black;
+    color: white;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+        background-color: white;
+        color: black;
+    }
+
+`
